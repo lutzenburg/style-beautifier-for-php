9452ae776ce716b91707c2474e451e6e4f5b42b6
Kellie Lutze <lutzenburg@users.noreply.github.com>
2017-01-13 10:16:52 +1000

    Update update_style.py

--- update_style.py
+++ update_style.py
@@ -2,12 +2,13 @@ import re
 import sys
 
 
-fileToParse = None
-if len(sys.argv) < 2:
-    fileToParse = input('Enter file you wish to parse: ')
-else:
-    fileToParse = sys.argv[1]
+# fileToParse = None
+# if len(sys.argv) < 2:
+#     fileToParse = input('Enter file you wish to parse: ')
+# else:
+#     fileToParse = sys.argv[1]
 
+fileToParse = '.\Lib\RateLimits.php'
 
 f = open(fileToParse, 'r+')
 content = f.read()
@@ -15,6 +16,11 @@ content = f.read()
 underScoreVariables = r"(\$[a-zA-Z]+)_([a-zA-Z]{1})([a-zA-Z]+)"
 classAttributeVariables = r"->([a-zA-Z]+)_([a-zA-Z]{1})([a-zA-Z]+)"
 bracketSpaces = r"([\S]*)[\s]*\([\s]*(.*)[\s]*\)"
+stringSpacing = r"(['|\"])\s*(\.)\s*(\$[a-zA-Z]+)(\.)\s*(['|\"])"
+publicVars = r"([public|private])(\s+)(?!\$this)\$([a-zA-Z]{1})"
+publicVarsComplete = r"[public|private]\s*\$([a-zA-Z]+);"
+classAttributeCamel = r"\$this->([a-zA-Z]+)"
+
 
 #  Convert underscore separated variables with camelcase
 content = re.sub(underScoreVariables, lambda pat: pat.group(1).lower() +
@@ -44,6 +50,19 @@ content = re.sub(r'([\t]*)(if).*\((\$[a-zA-Z]+)\)\s*({)([\s]{1})?',
 # ( test )
 content = re.sub(bracketSpaces, lambda pat: pat.group(1) +
                  "(" + pat.group(2).strip() + ")", content)
+
+# Fix string concat spacing
+content = re.sub(stringSpacing, lambda pat: pat.group(1) + " " + pat.group(2) + " " +
+                 pat.group(3) + " " + pat.group(4) + " " + pat.group(1), content)
+
+# Captitalise references to public variables
+for result in re.findall(classAttributeCamel, content):
+	if result in re.findall(publicVarsComplete, content):
+		 content = re.sub(r"(\$this->)(?=" + result + ")(\w{1})", lambda pat: pat.group(1) + pat.group(2).upper(), content)
+
+
+# Captitalise all public variables
+content = re.sub(publicVars, lambda pat: pat.group(1) + " $" + pat.group(3).upper(), content)
 
 f.seek(0)
 f.write(content)
